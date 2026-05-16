29b7ef5e5f6e24e8df97559ce56a8770a7d6596f
NaNo-MooN <aman-in-wu@outlook.com>
2026-05-16 17:26:23 +0800

    修复mason-lspconfig问题

--- lua/theconfig/plugins/mason-lspconfig.lua
+++ lua/theconfig/plugins/mason-lspconfig.lua
@@ -1,19 +1,8 @@
--- Mason 与 LSPConfig 桥接
-return {
-    "williamboman/mason-lspconfig.nvim",
+{
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
     dependencies = {
-        "williamboman/mason.nvim",
+        { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
-    config = function()
-        local mason_lspconfig = require("mason-lspconfig")
-        mason_lspconfig.setup()
-
-        -- 自动配置已安装的 LSP
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({})
-            end,
-        })
-    end,
 }
